0b0a2a0cc115334aef429266b634035d22e52d81
salihcodev <ahmedsalihdev@gmail.com>
2021-03-07 13:21:48 +0200

    Compose [starting the app & testing it] with docker-compose file. - Add compose file to automate running the the project and testing it in one command. - Create dockerfile to build the production and serve it with NGINX.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from DOCKER world;</p>
+        <p>DOCKER COMPOSE & development mode :) </p>
         <a
           className="App-link"
           href="https://reactjs.org"
